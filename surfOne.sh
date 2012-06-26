5cd88f8d9e6beebc87ba05d2b82e600cfbf33de1
Will Foran <will.foran@gmail.com>
2012-06-26 14:09:05 -0400

    help and checks

--- surfOne.sh
+++ surfOne.sh
@@ -1,20 +1,16 @@
 #!/usr/bin/env bash
 
-#
-# search for subjects in raw/MM
-# that do not have directory in MM/FS
-#   do reconall -all on these subjects
-# 
-#  input  <--  /data/Luna1/Raw/MultiModal/lunaid_date/*mprage*/*dcm
-#
-#  nii    -->  /data/Luna1/Multimodal/ANTI/${subjctid}/mprage
-#  FS     -->  /data/Luna1/Multimodal/FS_Subjects/${subjctid}  
-#
-#  FS log -->  /data/Luna1/Multimodal/FS_Subjects/${subjctid}/scripts/recon-all.log # longer log file
-#         -->  /data/Luna1/Multimodal/ANTI/${subjctid}/mprage/recon-all.log         # redirect of stdout/err
-#
-#
-
+## USAGE
+##     surfOne -i ID  -s SUBJECTS_DIR  -n NIFTI 
+##      subjects_dir can be omited if exported in environment
+## other options:
+##  -e email notifications from qsub (also EMAILS env var)
+##  -t FS-{TYPE}-subject qsub title 
+##  -d dcm dir (instead of -n), not implemented
+##END
+function helpmsg {
+   sed -n 's/^## //p;/##END/q' $0; exit 
+}
 
 case $HOSTNAME in
 *gromit*)
@@ -27,11 +23,11 @@ case $HOSTNAME in
   echo dont know what to do on $HOSTNAME && exit ;;
 esac
 
-# if using qsub, who to email about jobs
-# Directories
-#LUNADIR="/raid/r3/p2/Luna"
+# exit if no arguments
+[ -z "$1" ] && helpmsg
 
-while getopts 's:i:n:t:e:m:' switch; do
+# get options
+while getopts 's:i:n:t:e:h' switch; do
  case $switch in
     s) SUBJECTS_DIR=$OPTARG ;;
     i)     subjctid=$OPTARG ;;
@@ -39,6 +35,7 @@ while getopts 's:i:n:t:e:m:' switch; do
     t)         TYPE=$OPTARG ;;
     e)       EMAILS=$OPTARG ;;
     d)       dcmdir=$OPTARG ;;
+    *) helpmsg ;;
  esac
 done
 if [ -n "$dcmdir" ]; then
@@ -64,9 +61,15 @@ fi
 # remove lunadir from niifile so it can be replaced by
 # gromit specific path
 scriptloc=$(dirname $0)
+
+# check if finished
+tail -n1 ${SUBJECTS_DIR}/$subjctid/scripts/recon-all.log | grep 'without error' && echo "already finished without error" && exit 1
+# check if already in que
+qstat -f | grep FS-$TYPE-$subjctid && echo "FS-$TYPE-$subjctid already in que" && exit 1
+
 set -ex
  # use -h to hold by default
- echo qsub -m abe -M $EMAILS \
+ qsub -m abe -M $EMAILS \
       -e $scriptloc/log  -o $scriptloc/log \
       -N "FS-$TYPE-$subjctid" \
       -v subjctid="$subjctid",niifile="${niifile##$LUNADIR}",subjdir="${SUBJECTS_DIR##$LUNADIR}",TYPE="$TYPE" \
